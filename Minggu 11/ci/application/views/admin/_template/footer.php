d39dced0893a185c2762a6aefb87867cfd6833b7
C_E31191551_Calvinda Risky Adiputra_Web <riskycalvinda@gmail.com>
2021-04-19 07:17:18 +0700

    Minggu 11(Template dan CRUD)

---
+++ Minggu 11/ci/application/views/admin/_template/footer.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            Copyright &copy;
+            <script>
+                document.write(new Date().getFullYear());
+            </script> All rights reserved by Calvinda Risky</div>
+    </div>
+</footer>
